ffb90c6986a1af60d6574e0ec647804d061eed94
Melroy <meldsza@gmail.com>
2017-02-08 11:13:38 +0400

    Fixed Ghost

--- parser.js
+++ parser.js
@@ -2,16 +2,17 @@ module.exports.parse = (text) => {
 	let arr = text.trim().split('\n');
 	console.log(arr);
 	let head =null;
-	let end = arr.length;
-	for(let i=0;i<arr.length-1;i++)
+	console.log(arr);
+	for(let i=0;i<arr.length;i++)
 	{
 		arr[i] = arr[i].split('\t');
 		if(arr[i][0]=='H')
 			head = arr[i];
 		if(arr[i][0]=='$')
-		{end=i;}
+		{arr.splice(i,1)}
 	}
-	arr = arr.slice(arr.indexOf(head),end);
+	arr = arr.slice(arr.indexOf(head));
+	console.log(arr);
 	for(let i=0;i<arr.length;i++)
 	{
 		arr[i].shift();
