48885c2d01699ac1e698c09afcb65d9452e2d07d
Melroy <meldsza@gmail.com>
2017-02-08 11:01:56 +0400

    Hopefully fixed ghost player

--- parser.js
+++ parser.js
@@ -2,16 +2,16 @@ module.exports.parse = (text) => {
 	let arr = text.trim().split('\n');
 	console.log(arr);
 	let head =null;
+	let end = arr.length;
 	for(let i=0;i<arr.length-1;i++)
 	{
 		arr[i] = arr[i].split('\t');
 		if(arr[i][0]=='H')
 			head = arr[i];
 		if(arr[i][0]=='$')
-		{arr.splice(i,1)}
+		{end=i;}
 	}
-	arr.pop();
-	arr = arr.slice(arr.indexOf(head));
+	arr = arr.slice(arr.indexOf(head),end);
 	for(let i=0;i<arr.length;i++)
 	{
 		arr[i].shift();
