480b722db21b29e5577f699d380a7c37c4e7afbb
Melroy <melroynd@live.com>
2017-02-10 15:12:49 +0400

    fixed getPlayer method

--- bf2142.js
+++ bf2142.js
@@ -2,13 +2,13 @@ const request = require('request-promise');
 const parser = require('./parser');
 const getPlayers = (nick) => getAuthToken(0).then(auth => request(getOptions('http://s.bf2142.us/playersearch.aspx?nick=' + nick + '&auth=' + auth))
     .catch(console.log)
-    .then(parser.parse).then(toSoldiers));
+    .then(parser.parse).then(p => toSoldiers(p.arr,p.head)));
 const getLeaderBoard = (type, id, n) => getAuthToken(0).then(auth => request(getOptions('http://s.bf2142.us/getleaderboard.aspx?type=' + type + '&id=' + id + '&pos=0&after=' + n + '&auth=' + auth))
     .catch(console.log)
-    .then(parser.parse).then(toSoldiers));
+    .then(parser.parse).then(p =>toSoldiers(p.arr,p.head)));
 const getPlayer = (pid) => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getplayerinfo.aspx?auth=' + auth + '&mode=base'))
     .catch(console.log)
-    .then(parser.parse).then(toSoldier));
+    .then(body => parser.parse(body,2)).then(p =>toSoldier(p.arr,p.head)));
 const getOptions = function(URL) {
     return {
         url: URL,
@@ -23,27 +23,20 @@ const Soldier = function() {
     this.getAwards = () => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getawardsinfo.aspx?auth=' + auth))
         .catch(console.log)
         .then(parser.parse)
-        .then(getAwards));
+        .then(p=> getAwards(p.arr,p.head)));
     this.getUnlocks = () => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getunlocksinfo.aspx?auth=' + auth))
         .catch(console.log)
         .then(parser.parse)
-        .then(getunlocksinfo));
+        .then(p=>getunlocksinfo(p.arr,p.head)));
 };
-const toSoldiers = function(arr) {
+const toSoldiers = function(arr,head) {
 	if(!arr)
 	{return undefined;}
     let plist = new Array();
-    let head = arr[0];
-    arr = arr.slice(1);
     arr.map(p => plist.push(toSoldier(p,head)));
     return plist;
 };
 const toSoldier = function(p, head) {
-	if(!p)
-	{return undefined;}
-	if(!head)
-	{let head = p[0];
-    p = p.slice(1);}
     let s = new Soldier();
     for (let i = 0; i < p.length; i++) {
         s[head[i]] = p[i];
@@ -56,9 +49,7 @@ const modifySoldier = function(s, head, data) {
     }
     return s;
 };
-const getAwards = function(arr) {
-    let head = arr[0];
-    arr = arr.slice(1);
+const getAwards = function(arr,head) {
     let awards = [];
     arr.map(data => {
         let award = {};
@@ -69,9 +60,7 @@ const getAwards = function(arr) {
     });
     return awards;
 };
-const getunlocksinfo = function(arr) {
-    let head = arr[0];
-    arr = arr.slice(1);
+const getunlocksinfo = function(arr,head) {
     let unlocks = [];
     arr.map(data => {
         let unlock = {};
--- parser.js
+++ parser.js
@@ -1,20 +1,37 @@
-module.exports.parse = (text) => {
+module.exports.parse = (text,headcount) => {
 	let arr = text.trim().split('\n');
+	//console.log(text);
+	let c=0;
+	if(!headcount)
+	{headcount = 10;}
 	let head = undefined;
 	for(let i=0;i<arr.length;i++)
 	{
 		arr[i] = arr[i].split('\t');
 		if(arr[i][0]=='H')
-			head = arr[i];
+		{
+						                        if(c<headcount)                                    {                
+		    	 head = arr[i];					}
+		     else
+		     {
+			 arr.splice(i);
+			 break;
+		     }
+		     c++;
+		}
 		if(arr[i][0]=='$')
 		{arr.splice(i,1)}
 	}
 	if(head==undefined)
 	{return undefined;}
-	arr = arr.slice(arr.indexOf(head));
+	arr = arr.slice(arr.indexOf(head+1));
 	for(let i=0;i<arr.length;i++)
 	{
 		arr[i].shift();
 	}
-	return arr;
+	if(arr.length===1)
+	{arr= arr[0]}
+	head.shift();
+	//console.log(head);
+	return {'arr':arr,'head':head};
 }
