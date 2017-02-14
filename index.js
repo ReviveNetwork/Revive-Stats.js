599bbd5c92763f89443a9ed66266c43ae4d6f412
Melroy <melroynd@live.com>
2017-02-14 19:19:30 +0400

    removed test condition

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports.bf2 = require('./bf2');
 module.exports.bf2142 = require('./bf2142');
 
-//module.exports.bf2142.getPlayers('melroy').then(console.log);
+//module.exports.bf2.getPlayers('melroy').then(console.log);
