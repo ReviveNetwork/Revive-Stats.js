b5f5efb290b1be5ecd176fd110436bf12b0873e6
Melroy <melroynd@live.com>
2017-02-15 18:32:43 +0400

    changed key and iv

--- authToken.js
+++ authToken.js
@@ -1,15 +1,15 @@
 const crc = require('./lib/crc16-ccitt').crc16;
 const CryptoJS = require('crypto-js');
-const key = 'L\xbbV\xaax';
-const iv = '0';
+const key = new Buffer('4CBB56AA780000C365FFEF4423122C2C').toString('hex');
+const iv = '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0';
 module.exports.getToken = function(pid){
 	code = dwh(dechex(new Date().getTime()))+'640000'+dwh(dechex(pid))+"0000"; 
 	code += crc(code).toString().substr(0,4);
-	//console.log(code);
+	console.log(code);
 	code = CryptoJS.AES.encrypt(code, key, { iv: iv }).toString();
 	code = new Buffer(code).toString('base64');
 	code = code.replace(/=/g,'_').replace(/\//g,']').replace(/\+/g,'[');
-	//console.log(code);
+	console.log(code);
 	return code;	
 }
 function dwh(h)
