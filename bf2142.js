3d095bec8523efc89d72543d4bf6b2a9d014d8fa
Melroy <meldsza@gmail.com>
2017-02-15 12:27:24 +0400

    Fix conflicts

--- bf2142.js
+++ bf2142.js
@@ -3,7 +3,6 @@ const parser = require('./parser');
 const auth = require('./authToken.js').getToken;
 const getPlayers = (nick) => request(getOptions('http://s.bf2142.us/playersearch.aspx?nick=' + nick + '&auth=' + auth(0)))
     .catch(console.log)
-<<<<<<< HEAD
     .then(parser.parse).then(toSoldiers);
 const getLeaderBoard = (type, id, n) => request(getOptions('http://s.bf2142.us/getleaderboard.aspx?type=' + type + '&id=' + id + '&pos=0&after=' + n + '&auth=' + auth(0)))
     .catch(console.log)
@@ -11,15 +10,6 @@ const getLeaderBoard = (type, id, n) => request(getOptions('http://s.bf2142.us/g
 const getPlayer = (pid) => request(getOptions('http://s.bf2142.us/getplayerinfo.aspx?auth=' + auth(pid) + '&mode=base'))
     .catch(console.log)
     .then(parser.parse).then(toSoldier);
-=======
-    .then(parser.parse).then(p => toSoldiers(p.arr,p.head)));
-const getLeaderBoard = (type, id, n) => getAuthToken(0).then(auth => request(getOptions('http://s.bf2142.us/getleaderboard.aspx?type=' + type + '&id=' + id + '&pos=0&after=' + n + '&auth=' + auth))
-    .catch(console.log)
-    .then(parser.parse).then(p =>toSoldiers(p.arr,p.head)));
-const getPlayer = (pid) => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getplayerinfo.aspx?auth=' + auth + '&mode=base'))
-    .catch(console.log)
-    .then(body => parser.parse(body,2)).then(p =>toSoldier(p.arr[0],p.head))).catch(console.log);
->>>>>>> 599bbd5c92763f89443a9ed66266c43ae4d6f412
 const getOptions = function(URL) {
     return {
         url: URL,
@@ -34,19 +24,11 @@ const Soldier = function() {
     this.getAwards = () => request(getOptions('http://s.bf2142.us/getawardsinfo.aspx?auth=' + auth(this.pid)))
         .catch(console.log)
         .then(parser.parse)
-<<<<<<< HEAD
         .then(getAwards);
     this.getUnlocks = () => request(getOptions('http://s.bf2142.us/getunlocksinfo.aspx?auth=' + auth(this.pid)))
         .catch(console.log)
         .then(parser.parse)
         .then(getunlocksinfo);
-=======
-        .then(p=> getAwards(p.arr,p.head)));
-    this.getUnlocks = () => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getunlocksinfo.aspx?auth=' + auth))
-        .catch(console.log)
-        .then(parser.parse)
-        .then(p=>getunlocksinfo(p.arr,p.head)));
->>>>>>> 599bbd5c92763f89443a9ed66266c43ae4d6f412
 };
 const toSoldiers = function(arr,head) {
 	if(!arr)
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports.bf2 = require('./bf2');
 module.exports.bf2142 = require('./bf2142');
 
-<<<<<<< HEAD
 //module.exports.bf2142.getPlayer("1908093").then(console.log);
-=======
-//module.exports.bf2.getPlayers('melroy').then(console.log);
->>>>>>> 599bbd5c92763f89443a9ed66266c43ae4d6f412
+
