e724cd41c7c5ce976de176243fc9bbb94b0db88f
Melroy <melroynd@live.com>
2017-02-15 22:59:02 +0400

    change key

--- authToken.js
+++ authToken.js
@@ -1,12 +1,15 @@
 const crc = require('./lib/crc16-ccitt').crc16;
 const CryptoJS = require('crypto-js');
-const key = new Buffer('4CBB56AA780000C365FFEF4423122C2C').toString('hex');
+const key = '4CBB56AA780000C365FFEF4423122C2C';
+//const eas = require('./lib/eas');
 const iv = '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0';
 module.exports.getToken = function(pid){
 	code = dwh(dechex(new Date().getTime()))+'640000'+dwh(dechex(pid))+"0000"; 
 	code += crc(code).toString().substr(0,4);
 	console.log(code);
 	code = CryptoJS.AES.encrypt(code, key, { iv: iv }).toString();
+	//code = eas.DefEncryptBlock(eas.hex2str(code));
+	console.log(code);
 	code = new Buffer(code).toString('base64');
 	code = code.replace(/=/g,'_').replace(/\//g,']').replace(/\+/g,'[');
 	console.log(code);
