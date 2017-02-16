9351e3522b91eb1e690bf647832afdbba74be4cd
Melroy <meldsza@gmail.com>
2017-02-16 06:43:02 +0400

    Forgot to stage this earlier

--- bf2142.js
+++ bf2142.js
@@ -3,13 +3,13 @@ const parser = require('./parser');
 const auth = require('./authToken.js').getToken;
 const getPlayers = (nick) => request(getOptions('http://s.bf2142.us/playersearch.aspx?nick=' + nick + '&auth=' + auth(0)))
     .catch(console.log)
-    .then(parser.parse).then(toSoldiers);
+    .then(parser.parse).then(p => toSoldiers(p.arr,p.head));
 const getLeaderBoard = (type, id, n) => request(getOptions('http://s.bf2142.us/getleaderboard.aspx?type=' + type + '&id=' + id + '&pos=0&after=' + n + '&auth=' + auth(0)))
     .catch(console.log)
-    .then(parser.parse).then(toSoldiers);
+    .then(parser.parse).then(p => toSoldiers(p.arr,p.head));
 const getPlayer = (pid) => request(getOptions('http://s.bf2142.us/getplayerinfo.aspx?auth=' + auth(pid) + '&mode=base'))
     .catch(console.log)
-    .then(parser.parse).then(toSoldier);
+    .then(res => parser.parse(res,2)).then(p => toSoldier(p.arr[0],p.head));
 const getOptions = function(URL) {
     return {
         url: URL,
@@ -21,14 +21,14 @@ const getOptions = function(URL) {
 exports.getPlayers = getPlayers;
 const Soldier = function() {
     this.pid = 0;
-    this.getAwards = () => request(getOptions('http://s.bf2142.us/getawardsinfo.aspx?auth=' + auth(this.pid)))
+    this.getAwards = () => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getawardsinfo.aspx?auth=' + auth(pid)))
         .catch(console.log)
         .then(parser.parse)
-        .then(getAwards);
-    this.getUnlocks = () => request(getOptions('http://s.bf2142.us/getunlocksinfo.aspx?auth=' + auth(this.pid)))
+        .then(p=> getAwards(p.arr,p.head)));
+    this.getUnlocks = () => getAuthToken(pid).then(auth => request(getOptions('http://s.bf2142.us/getunlocksinfo.aspx?auth=' + auth(pid)))
         .catch(console.log)
         .then(parser.parse)
-        .then(getunlocksinfo);
+        .then(p=>getunlocksinfo(p.arr,p.head)));
 };
 const toSoldiers = function(arr,head) {
 	if(!arr)
