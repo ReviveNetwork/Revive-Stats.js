70a153e9d5d3f0bf3db652acaa45e5983c1821c8
meldsza <melroynd@live.com>
2017-02-16 20:52:19 +0400

    Fixed the playerlist

--- parser.js
+++ parser.js
@@ -1,16 +1,20 @@
 module.exports.parse = (text, headcount) => {
     let arr = text.trim().split('\n');
-    //console.log(text);
+    //console.log(arr);
     let c = 0;
     if (!headcount) {
         headcount = 10;
     }
     let head = undefined;
-    for (let i = 0; i < arr.length; i++) {
+	let i=0;
+	let hi=0;
+    for (i = 0; i < arr.length; i++) {
         arr[i] = arr[i].split('\t');
-        if (arr[i][0] == 'H') {
+		//console.log(arr[i]);
+        if (arr[i][0] === 'H') {
             if (c < headcount) {
                 head = arr[i];
+				hi = i;
             } else {
                 arr.splice(i);
                 break;
@@ -18,13 +22,14 @@ module.exports.parse = (text, headcount) => {
             c++;
         }
         if (arr[i][0] == '$') {
-            arr.splice(i, 1)
+            arr.splice(i,1)
         }
     }
     if (head == undefined) {
         return {};
     }
-    arr = arr.slice(arr.indexOf(head + 1));
+    arr = arr.slice(hi+1);
+	//console.log(arr);
     for (let i = 0; i < arr.length; i++) {
         arr[i].shift();
     }
