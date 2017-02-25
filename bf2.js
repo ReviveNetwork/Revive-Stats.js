f1c518db0f3272eed64826e3cce1dcd080a0cf59
Melroy <meldsza@gmail.com>
2017-02-25 15:40:39 +0400

    Added better accessors

--- bf2.js
+++ bf2.js
@@ -2,22 +2,21 @@ const request = require('request-promise');
 const parser = require('./parser');
 const getPlayers = (nick) => request(getOptions('http://bf2web.game.bf2.us/ASP/searchforplayers.aspx?nick=' + nick + '&where=a&sort=a&debug=txs&transpose=0'))
     .catch(console.log)
-    .then(parser.parse).then(p => toSoldiers(p.arr,p.head)).then(p => p.sort(function(a, b)
-	{
-		let aStart = a.nick.match(new RegExp('^'+nick, 'i')) || [],
-         bStart = b.nick.match(new RegExp('^'+nick, 'i')) || [];
+    .then(parser.parse).then(p => toSoldiers(p.arr, p.head)).then(p => p.sort(function (a, b) {
+        let aStart = a.nick.match(new RegExp('^' + nick, 'i')) || [],
+            bStart = b.nick.match(new RegExp('^' + nick, 'i')) || [];
 
-		if ( aStart.length != bStart.length ) return bStart.length - aStart.length;
+        if (aStart.length != bStart.length) return bStart.length - aStart.length;
 
-		else return a.nick > b.nick ? 1 : -1;
-	}));
+        else return a.nick > b.nick ? 1 : -1;
+    }));
 const getLeaderBoard = (type, id, n) => request(getOptions('http://bf2web.game.bf2.us/ASP/getleaderboard.aspx?type=' + type + '&id=' + id + 'after=' + n))
     .catch(console.log)
-    .then(parser.parse).then(p => toSoldiers(p.arr,p.head));
+    .then(parser.parse).then(p => toSoldiers(p.arr, p.head));
 const getPlayer = (pid) => request(getOptions('http://bf2web.game.bf2.us/ASP/getplayerinfo.aspx?pid=' + pid + '&info=per*,cmb*,twsc,cpcp,cacp,dfcp,kila,heal,rviv,rsup,rpar,tgte,dkas,dsab,cdsc,rank,cmsc,kick,kill,deth,suic,ospm,klpm,klpr,dtpr,bksk,wdsk,bbrs,tcdr,ban,dtpm,lbtl,osaa,vrk,tsql,tsqm,tlwf,mvks,vmks,mvn*,vmr*,fkit,fmap,fveh,fwea,wtm-,wkl-,wdt-,wac-,wkd-,vtm-,vkl-,vdt-,vkd-,vkr-,atm-,awn-,alo-,abr-,ktm-,kkl-,kdt-,kkd-'))
     .catch(console.log)
-    .then(parser.parse).then(p => toSoldier(p.arr[0],p.head));
-const getOptions = function(URL) {
+    .then(parser.parse).then(replace).then(p => toSoldier(p.arr[0], p.head));
+const getOptions = function (URL) {
     return {
         url: URL,
         headers: {
@@ -26,40 +25,40 @@ const getOptions = function(URL) {
     };
 };
 exports.getPlayers = getPlayers;
-const Soldier = function() {
+const Soldier = function () {
     this.pid = 0;
     this.getAwards = () => request(getOptions('http://bf2web.game.bf2.us/ASP/getawardsinfo.aspx?pid=' + pid))
         .catch(console.log)
         .then(parser.parse)
-        .then(p => getAwards(p.arr,p.head));
+        .then(p => getAwards(p.arr, p.head));
     this.getUnlocks = () => request(getOptions('http://bf2web.game.bf2.us/ASP/getunlocksinfo.aspx?pid=' + pid))
         .catch(console.log)
         .then(parser.parse)
-        .then(p => getunlocksinfo(p.arr,p.head));
+        .then(p => getunlocksinfo(p.arr, p.head));
 };
-const toSoldiers = function(arr,head) {
-	if(!arr)
-	{return undefined;}
+const toSoldiers = function (arr, head) {
+    if (!arr)
+    { return undefined; }
     let plist = new Array();
-    arr.map(p => plist.push(toSoldier(p,head)));
+    arr.map(p => plist.push(toSoldier(p, head)));
     return plist;
 };
-const toSoldier = function(p, head) {
-	if(!p)
-	{return p;}
+const toSoldier = function (p, head) {
+    if (!p)
+    { return p; }
     let s = new Soldier();
     for (let i = 0; i < p.length; i++) {
         s[head[i]] = p[i];
     }
     return s;
 };
-const modifySoldier = function(s, head, data) {
+const modifySoldier = function (s, head, data) {
     for (let i = 0; i < data.length; i++) {
         s[head[i]] = data[i];
     }
     return s;
 };
-const getAwards = function(arr,head) {
+const getAwards = function (arr, head) {
     let awards = [];
     arr.map(data => {
         let award = {};
@@ -70,7 +69,7 @@ const getAwards = function(arr,head) {
     });
     return awards;
 };
-const getunlocksinfo = function(arr,head) {
+const getunlocksinfo = function (arr, head) {
     let unlocks = [];
     arr.map(data => {
         let unlock = {};
@@ -81,6 +80,14 @@ const getunlocksinfo = function(arr,head) {
     });
     return unlocks;
 };
+const replace = (p) => {
+    const data = require('./bf2head.json');
+    for (let i = 0; i < p.head.length; i++) {
+        if (data[p.head[i]])
+            p.head = data[p.head[i]];
+    }
+    return p;
+}
 module.exports.getPlayer = getPlayer;
 module.exports.getPlayers = getPlayers;
 module.exports.getLeaderBoard = getLeaderBoard;
