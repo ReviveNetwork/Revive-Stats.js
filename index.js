cf54421c9d4ccf0ce94cf1f9d894c710577e58ac
meldsza <melroynd@live.com>
2017-03-01 18:08:34 +0400

    remove tests

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var stats = {
     auth: require('./authToken')
 }
 module.exports = stats;
-var fs = require('fs');
+//var fs = require('fs');
 //module.exports.bf2.getPlayer("1111542").then(p => fs.writeFile("out.json", JSON.stringify(p), console.log));
-module.exports.bf2142.getPlayer("1908093").then(p => fs.writeFile("out.json", JSON.stringify(p), console.log));
+//module.exports.bf2142.getPlayer("1908093").then(p => fs.writeFile("out.json", JSON.stringify(p), console.log));
 //console.log(require('./authToken.js').parseToken('bSCZgQN08cGhavVXAhY7CA__'));
 //console.log(module.exports.auth.getToken('1908093'))
